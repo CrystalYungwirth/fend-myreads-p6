8de2d3dcb89eee8da808d4f23f2efbfa9003bede
Crystal Yungwirth <CrystalYungwirth@gmail.com>
2018-09-27 14:37:18 +0000

    feat: close search link component

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -12,7 +12,7 @@ export default class SearchPage extends Component {
     	return(
           <div className="search-books">
           	<div className="search-books-bar">
-          		<a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+          	  <CloseSearch />
 				<div className="search-books-input-wrapper">
                 {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
