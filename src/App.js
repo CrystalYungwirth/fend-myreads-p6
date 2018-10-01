cc2cca30088f9fcbdc4f6239e9ff1f43a897f8c5
Crystal Yungwirth <CrystalYungwirth@gmail.com>
2018-10-01 19:46:26 +0000

    refactor: updateBookshelf using prevShelf and so not calling gethAll() API twice

--- src/App.js
+++ src/App.js
@@ -38,10 +38,9 @@ export default class BooksApp extends React.Component {
     this.getAllBooks();
   }
 
-  updateBookshelf = (book, shelf) => {
-    BooksAPI.update(book, shelf).then(() => {
-      this.getAllBooks();
-    })
+  updateBookshelf = (prevShelf, shelf) => {
+    BooksAPI.update(prevShelf, shelf).then(() => {
+      })
   }
 
   render() {
