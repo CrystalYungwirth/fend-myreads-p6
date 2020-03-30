95dba2165b0a05c4b52b2bc13359a3d9bfe42b28
Crystal Yungwirth <crystalyungwirth@gmail.com>
2020-03-30 11:44:23 -0400

    fix: shelf update after search selection

--- src/App.js
+++ src/App.js
@@ -37,21 +37,14 @@ export default class BooksApp extends React.Component {
     this.getAllBooks();
   }
 
-//This was a suggestion in my last review. But now whenever I add a book from the search page the MyReads page needs reloaded
-//before the book shows up. I'm at a loss for how come. Everything seems to be the same between the two pages. I read a lot to
-//figure this out, such as: https://stackoverflow.com/questions/51118865/setstate-in-an-array-with-react-spread-operator. I'm not 
-//sure I'm using the componentDidUpdate correctly, it doesn't throw an error but it doesn't seem to do anything either. 
-  updateBookshelf = (prevShelf, shelf) => {
+updateBookshelf = (prevShelf, shelf) => {
 	prevShelf.shelf = shelf    
     BooksAPI.update(prevShelf, shelf)
       .then(books => {
-        this.setState((state) => ({
-        books: state.books.filter((newShelf) => 
-               newShelf.id !== prevShelf.id).concat([ prevShelf ])
-      }))
-    })
-  }
-
+        this.getAllBooks();
+      })
+    }
+  
   render() {
     return (
       <BrowserRouter>
